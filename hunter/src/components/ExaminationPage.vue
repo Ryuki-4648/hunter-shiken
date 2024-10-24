04a05e17d1624bb0e6b20240c6c9e949bef29f79
Ryuki-4648 <hato_poppo73@yahoo.co.jp>
2024-10-24 23:46:41 +0900

    問題トップ、結果ページ

---
+++ hunter/src/components/ExaminationPage.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <div>
+      <h1>HUNTER試験</h1>
+      <p>テスト問題</p>
+      <p><span>【current】</span>/【total】問目</p>
+
+      <p>選択肢</p>
+      <ul class="">
+        <li class=""></li>
+        <li class=""></li>
+        <li class=""></li>
+        <li class=""></li>
+      </ul>
+
+      <button>次の問題へ</button>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+</style>
---
+++ hunter/src/components/ResultPage.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <div>
+      <h1>HUNTER試験</h1>
+      <p>試験の結果です</p>
+
+      <p><span>【total】</span>問中<span>【correct】</span>問正解しました。</p>
+
+      <ul class="">
+        <li class="">
+          <p>テスト問題</p>
+          <p>選択肢：<span>【選んだ記号】</span></p>
+          <p>【合否】</p>
+        </li>
+        <li class="">
+          <p>テスト問題</p>
+          <p>選択肢：<span>【選んだ記号】</span></p>
+          <p>【合否】</p>
+        </li>
+        <li class="">
+          <p>テスト問題</p>
+          <p>選択肢：<span>【選んだ記号】</span></p>
+          <p>【合否】</p>
+        </li>
+      </ul>
+
+      <button>トップに戻る</button>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+</style>
